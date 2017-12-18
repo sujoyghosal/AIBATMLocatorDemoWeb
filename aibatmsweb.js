a98916e7146dd943ad00cd448daa3ff66f8ade1c
sujoyghosal <sujoy.ghosal@gmail.com>
2017-12-18 16:59:17 +0530

    beautify

--- aibatmsweb.js
+++ aibatmsweb.js
@@ -6,6 +6,6 @@ app.use(express.static('www'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/www/index.html'));
 })
-var PORT = process.env.VCAP_APP_PORT || 8000;
+var PORT = process.env.VCAP_APP_PORT || 7000;
 app.listen(PORT);
 console.log('Server is listening to http://localhost/ on port ' + PORT);
--- apigeedeploy.bat
+++
@@ -1,1 +0,0 @@
-apigeetool deploynodeapp -n AIB_ATM_WEB -d . -m aibatmsweb.js -o sujoyghosal -e test -b /aibatmsweb -u gsujoy@yahoo.com -p Kolkata41
