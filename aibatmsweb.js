f924d80d78d964e0d6fcc9f65ddac9c8c002334e
sujoyghosal <sujoy.ghosal@gmail.com>
2017-12-18 12:55:16 +0530

    first commit

---
+++ aibatmsweb.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.use(express.static('www'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/www/index.html'));
+})
+var PORT = process.env.VCAP_APP_PORT || 8000;
+app.listen(PORT);
+console.log('Server is listening to http://localhost/ on port ' + PORT);
